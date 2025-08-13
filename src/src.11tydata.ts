4f680013b950625b0935b6b2e2c1149e71127678
xonin-hush <xonindev@gmail.com>
2025-08-13 19:13:23 +0300

    Fix Vercel deployment: Remove yarn.lock, update scripts to use npm, add vercel.json

--- src/src.11tydata.ts
+++ src/src.11tydata.ts
@@ -1,8 +1,6 @@
 import { enhance11tydata } from './util/i18n';
-import locales from './_data/locales.json';
 
 module.exports = () => {
-  // the root page is using the layout for all the site metadata, so better localise it
-  // XXX: should we use a custom minimal page for root page? any impacts on SEO?
-  return enhance11tydata({}, locales[0].path);
+  // Use Arabic as the root page locale
+  return enhance11tydata({}, 'ar', 'rtl');
 };
